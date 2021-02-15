36920ac2226986e89a3e708982784f5390901f34
Jared Pochtar <jpochtar@gmail.com>
2021-02-14 19:53:21 -0500

    monkey patching

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const frt_variable_name = "__frt_hook__";
 const frt_hooks = window[frt_variable_name] || {};
 
 export const useFRTAction = frt_hooks.useFRTAction || function() {};
-export const useFRTAssert = frt_hooks.useFRTAssert || function() {};
+export const useFRTActionMulti = frt_hooks.useFRTActionMulti || function() {};
 export const SkipFRT = frt_hooks.SkipFRT || React.Fragment;
+export const frtPatchComponent = frt_hooks.frtPatchComponent || function() {};
 
 
 /*** exploration utils ***/
@@ -15,4 +16,8 @@ export function pick_random(opts) {
     throw new Error("no options to choose from");
   }
   return opts[Math.floor(Math.random() * opts.length)];
+}
+
+export function useFRTActionIf(condition, action) {
+  useFRTActionMulti(!condition ? [] : [action]);
 }
